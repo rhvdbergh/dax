ec7590afc47cf94f7c71c2d9c50ae4a03f850551
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-09-06 20:33:42 -0400

    halfway setting up node as file server

--- js/nodelibs.js
+++ js/nodelibs.js
@@ -1,3 +1,24 @@
+//function as a file server on localhost:8080
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function(req, res) {
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, { 'content-type': 'text/html' });
+            return res.end("404 not found");
+        }
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write(data);
+        return res.end;
+
+    });
+}).listen(8080);
+
+
+// Create connection to MySQL database;
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
