fd0414e5432f1cf1848a655ee83e1bd9d6c36bae
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-09-21 20:26:57 -0400

    moved timer out of getwords section in learn.html

--- js/learn.js
+++ js/learn.js
@@ -1,9 +1,12 @@
 var seconds = 0;
+var timeUp = false;
+var shortTimeUp = false;
+var jsonObj;
 
 $(document).ready(function() {
     // when review.html is loaded, retrieve 
     $.get("?getwords", function(data) {
-        var jsonObj = JSON.parse(data);
+        jsonObj = JSON.parse(data);
 
         // set up initial cards
         $('.review_word_front').append(jsonObj[0].front);
@@ -13,17 +16,25 @@ $(document).ready(function() {
             $('.review_word_back').text(jsonObj[0].back);
         });
 
-        function timer() {
-            setInterval(function() {
-                seconds++;
-                $('.timerstr').text(Math.floor(seconds / 60) + ":" + (seconds % 60));
-            }, 1000);
-        }
+        $('#yes_btn').on('click', function() {
 
-        timer();
+        });
 
         console.log("request data returned");
         console.log("data is: " + data);
         console.log("The number of cards returned are: " + jsonObj.length)
+
+
     });
+
+    function timer() {
+        setInterval(function() {
+            seconds++;
+            $('.timerstr').text(Math.floor(seconds / 60) + ":" + (seconds % 60));
+        }, 1000);
+    }
+
+    timer();
+
+
 });
