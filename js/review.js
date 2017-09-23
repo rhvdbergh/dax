97110929878899a9fd3ff5e0ec69072335894405
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-09-22 21:28:22 -0400

    review implemented

--- js/review.js
+++ js/review.js
@@ -1,10 +1,158 @@
 $(document).ready(function() {
-    // when review.html is loaded, retrieve 
-    $.get("?getwords", function(data) {
-        var jsonObj = JSON.parse(data);
-        $('.review_word_front').append(jsonObj[0].front);
-        $('.review_word_back').append(jsonObj[0].back);
-        console.log("request data returned");
-        console.log("data is: " + data);
-    });
+
+    var cardFlipped = false;
+    var decisionMade = false;
+    // if there are less than 20 overdue cards left - i.e. returned from the server,
+    // it will be the last round of revisions
+    var lastRound = false;
+    var jsonObj;
+    var place = 0; // keeps track of which card is currently being learned
+
+    function calculateTimestamp() {
+        return Math.round(Date.now() / 1000);
+    }
+
+    function getNewWords() {
+        $.get("?getwords", function(data) {
+
+            // save present card, so user does not go out of sync
+            var currentCard = jsonObj[place];
+            jsonObj = JSON.parse(data);
+
+            place = 0;
+
+            console.log("Request for new data has returned!");
+            console.log("data is: " + data);
+            console.log("The number of cards returned are: " + jsonObj.length);
+
+            // check if the currentCard is among the cards returned
+            // if so, remove the card from the returned cards and place it "on top" at place 0
+            var currentCardReturned = false; // check to see if currentCard is among cards returned
+            for (var i = 0; i < jsonObj.length; i++) {
+                if (jsonObj[i].id === currentCard.id) {
+                    console.log("Old card already in set returned, card is: " + currentCard.id + " position is " + i);
+                    jsonObj.splice(i, 1); // take the currentCard out of the pile
+                    jsonObj.splice(0, 0, currentCard); // place currentCard at place 0
+
+                    currentCardReturned = true;
+                    console.log("After splice / insert, set of cards = " + JSON.stringify(jsonObj));
+                }
+            }
+
+            if (!currentCardReturned) { // currentCard was not among the batch returned
+                jsonObj[0] = currentCard; // so replace the first card at place 0 with currentCard
+            }
+
+            console.log("jsonObj.length = " + jsonObj.length);
+            if (jsonObj.length < 20) {
+                console.log("There are less than 20 cards left!");
+                lastRound = true;
+            }
+        });
+    }
+
+    function retrieveCards(callback) {
+        $.get("?getwords", function(data) {
+            jsonObj = JSON.parse(data);
+            console.log("request data returned");
+            console.log("data is: " + data);
+            console.log("The number of cards returned are: " + jsonObj.length);
+
+            console.log("jsonObj.length = " + jsonObj.length);
+            if (jsonObj.length < 20) {
+                console.log("There are less than 20 cards left!");
+                lastRound = true;
+            }
+
+            // set up initial cards
+
+            $('.review_word_front').text(jsonObj[place].front);
+            $('.review_word_back').text("Click here to reveal card...");
+            $('.review_question').hide();
+
+            $('.review_word_back').on('click', function() {
+
+                // check to see if new words are needed from the server
+                if (place > 17) {
+                    console.log("Space getting tight!");
+                    getNewWords();
+                }
+
+                if (!decisionMade) {
+                    $('.review_word_back').text(jsonObj[place].back);
+                    cardFlipped = true;
+                    $('.review_question').show();
+                }
+            });
+
+            callback();
+        });
+    };
+
+    function learnCards() {
+
+        $('#yes_btn').on('click', function() {
+            if (!decisionMade && cardFlipped) {
+
+                jsonObj[place].batch++;
+                jsonObj[place].overdue = 0;
+                jsonObj[place].timestamp = calculateTimestamp().toString();
+                $.get("?updatecard" + JSON.stringify(jsonObj[place]));
+
+                place++;
+
+                // make sure that jsonObj contains an object at place
+                // else this was the last card!
+                if (!(jsonObj.length - 1 < place)) {
+                    $('.review_word_front').text(jsonObj[place].front);
+                    $('.review_word_back').text("Click here to reveal card...");
+                    decisionMade = false;
+                    cardFlipped = false;
+                } else { // this was the last card!
+                    $('.review_word_front').text("All done! That was the last card.");
+                    $('.review_word_back').hide();
+                }
+
+                $('.review_question').hide();
+
+                // for debugging purposes:
+                console.log("Decision made: yes");
+                console.log("The card has been updated to: " + JSON.stringify(jsonObj[place - 1]));
+                console.log("Place: " + place);
+            }
+        });
+
+        $('#no_btn').on('click', function() {
+            if (!decisionMade && cardFlipped) {
+
+                jsonObj[place].batch = 0;
+                jsonObj[place].overdue = 0;
+                jsonObj[place].timestamp = calculateTimestamp().toString();
+                $.get("?updatecard" + JSON.stringify(jsonObj[place]));
+
+                place++;
+
+                // make sure that jsonObj contains an object at place
+                // else this was the last card!
+                if (!(jsonObj.length - 1 < place)) {
+                    $('.review_word_front').text(jsonObj[place].front);
+                    $('.review_word_back').text("Click here to reveal card...");
+                    cardFlipped = false;
+                    decisionMade = false;
+                } else { // this was the last card!
+                    $('.review_word_front').text("All done! That was the last card.");
+                    $('.review_word_back').hide();
+                }
+
+                $('.review_question').hide();
+
+                // for debugging purposes:
+                console.log("Decision made: no");
+                console.log("The card has been updated to: " + JSON.stringify(jsonObj[place - 1]));
+            }
+        });
+    }
+
+    retrieveCards(learnCards);
+
 });
