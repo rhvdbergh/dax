43fefca5013a65cebc511d64b0e820ce93eb311a
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-09-25 22:44:26 -0400

    landing page is login screen

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
+var loggedIn = false;
+
 $(document).ready(function() {
-/*
-    // on submission of a new word, reset input fields
-    $('#add_word_form').on('submit', function(e) {
+    if (!loggedIn) {
+        $('#nav_btn_add').hide();
+        $('#nav_btn_learn').hide();
+        $('#nav_btn_review').hide();
+        $('#nav_btn_account').hide();
+    }
 
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        setTimeout(function() { // setTimeout: make sure that the submission is delivered to the server before clearing values
+    $('#submit_login_btn').on('click', function() {
 
-            $('#front_input').val('');
-            $('#back_input').val('');
-            $('#front_input').focus();
-        });
-    }); */
+
+        // send login details to the server
+        // at this stage, the server will create a new user if no email address
+        // exists; in the future, there will be a sign-up form
+        console.log('Submitted!');
+    });
+
+    /*
+        // on submission of a new word, reset input fields
+        $('#add_word_form').on('submit', function(e) {
+
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            setTimeout(function() { // setTimeout: make sure that the submission is delivered to the server before clearing values
+
+                $('#front_input').val('');
+                $('#back_input').val('');
+                $('#front_input').focus();
+            });
+        }); */
 });
