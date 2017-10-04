c0626b289c63e62e45671c8f4e4efc30566063d3
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-10-03 23:20:30 -0400

    app.js generates jwt token with 7 days expiration - no validation yet

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var fs = require('fs');
 var url = require('url');
 var mysql = require('mysql');
 var qs = require('querystring');
+var jwt = require('jsonwebtoken');
 
-var usersInSession = []; // array to hold users in session
-
-// a session not interacted with for 5 minutes will expire 
-var sessionDuration = 900000; // should be 900000 (ms! = 15 minutes), if not this is for debugging
-
+// example of how to use jwt
+var token = jwt.sign({ 'foo': 'bar', }, 'shhhh');
+console.log('Token: ' + token);
+var decoded = jwt.verify(token, 'shhhh');
+console.log('Decoded token: ' + decoded.foo);
 
 // current working mysql database and table
+// for debugging purposes
 var currentDatabase = "temp_database";
 var currentTable = "words";
 
@@ -29,25 +31,38 @@ con.connect(function(err) {
 });
 
 // create a database if it doesn't exist, then select it
-var sql = "CREATE DATABASE IF NOT EXISTS " + currentDatabase;
-con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Database " + currentDatabase + " exists.");
-});
+function createDB(dbName, callback) {
+    var sql = "CREATE DATABASE IF NOT EXISTS " + dbName;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Database " + dbName + " exists.");
+        callback;
+    });
+}
 
-var sql = "USE " + currentDatabase;
-con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Database " + currentDatabase + " selected for use.");
-});
+function useDB(dbName, callback) {
+    var sql = "USE " + dbName;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Database " + dbName + " selected for use.");
+        callback;
+    });
+}
+
+createDB(currentDatabase, useDB(currentDatabase));
 
 // create a table if it doesn't exist
 // overdue column functions as a boolean column where 0 = false, 1 = true;
-var sql = "CREATE TABLE IF NOT EXISTS " + currentTable + " (id INT NOT NULL AUTO_INCREMENT, front TINYTEXT, back TINYTEXT, batch TINYINT DEFAULT 0, timestamp INT, overdue INT DEFAULT 0, PRIMARY KEY (id))"
-con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Table " + currentTable + " exists.");
-});
+function createTable(tableName, callback) {
+    var sql = "CREATE TABLE IF NOT EXISTS " + tableName + " (id INT NOT NULL AUTO_INCREMENT, front TINYTEXT, back TINYTEXT, batch TINYINT DEFAULT 0, timestamp INT, overdue INT DEFAULT 0, PRIMARY KEY (id))"
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Table " + tableName + " exists.");
+        callback;
+    });
+}
+
+createTable(currentTable);
 
 // helper-function for building sql string for calculating overdue timestamps
 function buildBatchSQLQuery(myBatch, time) {
@@ -100,9 +115,9 @@ function buildBatchSQLQuery(myBatch, time) {
 // 3 years (94608000 seconds)
 
 // function to run through the table once and update all the timestamps that are overdue
-function calculateOverdue() {
+function calculateOverdue(tableName, callback) {
     currentTime = calculateTimestamp();
-    var sql = "UPDATE " + currentTable + " SET overdue = 1 WHERE (";
+    var sql = "UPDATE " + tableName + " SET overdue = 1 WHERE (";
     // loop through batches and build sql query
     for (var i = 0; i < 7; i++) {
         sql += buildBatchSQLQuery(i, currentTime) + ") OR (";
@@ -112,7 +127,8 @@ function calculateOverdue() {
 
     con.query(sql, function(err, results, fields) {
         if (err) throw err;
-        console.log("Overdue words in table " + currentTable + " updated.");
+        console.log("Overdue words in table " + tableName + " updated.");
+        callback;
     });
 
 }
@@ -122,27 +138,27 @@ function calculateTimestamp() {
 }
 
 // calculate overdue timestamps and update "overdue" column in MySQL
-calculateOverdue();
+calculateOverdue(currentTable);
 
 // this function returns the number of words in the table without having to count
 // rather, the information is retrieved once from the INFORMATION_SCHEMA.TABLES table
-function getNumWords() {
+function getNumWords(dbName, tableName, callback) {
 
     var numWords = 0;
 
-    var sql = "SELECT table_rows FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + currentDatabase + "' AND table_name = '" + currentTable + "'";
+    var sql = "SELECT table_rows FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + dbName + "' AND table_name = '" + tableName + "'";
 
     con.query(sql, function(err, result) {
         if (err) throw err;
 
-        console.log("Number of words in " + currentTable + " table: " + result[0].table_rows);
+        console.log("Number of words in " + tableName + " table: " + result[0].table_rows);
         numWords = result[0].table_rows;
 
     });
     return numWords;
 }
 
-getNumWords();
+getNumWords(currentDatabase, currentTable);
 
 // act as a file server on localhost:8080
 // the file server will handle MySQL too
@@ -159,9 +175,9 @@ http.createServer(function(req, res) {
     }
 
     // this function retrieves a random overdue card from the database to review
-    function getRandomOverdueCards(callback) {
+    function getRandomOverdueCards(tableName, callback) {
         // if there are less than 20 overdue cards, only the overdue cards will be returned
-        var sql = "SELECT * FROM " + currentTable + " WHERE overdue = 1 ORDER BY RAND() LIMIT 20";
+        var sql = "SELECT * FROM " + tableName + " WHERE overdue = 1 ORDER BY RAND() LIMIT 20";
         con.query(sql, function(err, result) {
             if (err) throw err;
             console.log("Your random cards are: " + JSON.stringify(result));
@@ -169,9 +185,9 @@ http.createServer(function(req, res) {
         });
     }
 
-    function getRandomBatch0Cards(callback) {
+    function getRandomBatch0Cards(tableName, callback) {
         // if there are less than 20 batch 0 cards, only the correct number of cards will be returned
-        var sql = "SELECT * FROM " + currentTable + " WHERE batch = 0 ORDER BY RAND() LIMIT 20";
+        var sql = "SELECT * FROM " + tableName + " WHERE batch = 0 ORDER BY RAND() LIMIT 20";
         con.query(sql, function(err, result) {
             if (err) throw err;
             console.log("Your random cards are: " + JSON.stringify(result));
@@ -183,18 +199,18 @@ http.createServer(function(req, res) {
     if ((req.url.indexOf('?getoverduewords') != -1) && req.method === "GET") { // test if query was submitted
 
         console.log("request received");
-        getRandomOverdueCards(sendCards);
+        getRandomOverdueCards(currentTable, sendCards);
     }
 
     if ((req.url.indexOf('?getnewwords') != -1) && req.method === "GET") { // test if query was submitted
 
         console.log("request received");
-        getRandomBatch0Cards(sendCards);
+        getRandomBatch0Cards(currentTable, sendCards);
     }
 
-    function updateCard(card) {
+    function updateCard(tableName, card) {
         console.log("JSON passed to updateCard: " + JSON.stringify(card));
-        sql = "UPDATE " + currentTable + " SET batch = " + card.batch + ", timestamp = " + card.timestamp + ", overdue = " + card.overdue + " WHERE id = " + card.id;
+        sql = "UPDATE " + tableName + " SET batch = " + card.batch + ", timestamp = " + card.timestamp + ", overdue = " + card.overdue + " WHERE id = " + card.id;
         con.query(sql, function(err, result) {
             if (err) throw err;
             console.log("Card updated!");
@@ -213,33 +229,9 @@ http.createServer(function(req, res) {
         str = str_withoutQuote.replace(/%20/g, " ");
         console.log("JSON string received: " + str);
 
-        updateCard(JSON.parse(str));
+        updateCard(currentTable, JSON.parse(str));
 
         res.end();
-    }
-
-    function triggerSession(user) {
-        user.intervalID = setInterval(function() {
-
-            if (usersInSession.includes(user)) {
-                var userIndex = usersInSession.indexOf(user);
-                if (calculateTimestamp() - usersInSession[userIndex].lastInteraction > (sessionDuration / 1000)) { // user has been inactive for 6000 minutes
-                    // find position of usersInSession and remove user
-                    console.log("Session expired. Logging user " + user.userName + " out.");
-                    usersInSession.splice(userIndex, 1);
-                    clearInterval(user.intervalID);
-                    if (usersInSession.length > 0) {
-                        console.log("Users still in session: ");
-                        for (var i = 0; i < usersInSession.length; i++) {
-                            console.log("User " + usersInSession[i].userName);
-                        }
-                        console.log("----------------------");
-                    } else { console.log("No users left in session.") }
-                } else {
-                    console.log("User " + user.userName + " is still logged in.");
-                }
-            }
-        }, sessionDuration);
     }
 
     // File handling
@@ -247,7 +239,13 @@ http.createServer(function(req, res) {
     // test if method used is post
     if (req.method === "POST") {
         console.log("Request url: " + req.url); // for debugging purposes
-        fs.readFile('.' + req.url, function(err, data) {
+        var readurl = '';
+        if (req.url === '/' || req.url === '/index') {
+            readurl = './index.html';
+        } else {
+            readurl = '.' + req.url;
+        }
+        fs.readFile(readurl, function(err, data) {
             if (err) {
                 res.writeHead(404, { 'content-type': 'text/html' });
                 return res.end("404 not found");
@@ -271,6 +269,7 @@ http.createServer(function(req, res) {
                     console.log("Will now write to MySQL database");
                     console.log("Front text is: " + post.front_text);
                     console.log("Back text is: " + post.back_text);
+                    console.log("Key is: " + post.key);
 
                     // MySQL query handling
                     var sql = "INSERT INTO " + currentTable + " (front, back, timestamp) VALUES ('" + post.front_text + "', '" + post.back_text + "', '" + calculateTimestamp().toString() + "')";
@@ -278,27 +277,35 @@ http.createServer(function(req, res) {
                         if (err) throw err;
                         console.log("MySQL command: " + sql);
                     });
+
+                    // return the user to the web page selected above (html/add.html):
+                    res.writeHead(200, { 'content-type': 'text/html' });
+                    res.write(data);
+                    return res.end();
+
                     // else test if POST request was to log in
                     // if the parsed data contains a .uname field, this must be a login attempt
                 } else if (post.uname) {
                     console.log("Login request received.")
                     console.log("User login query received. Username: " + post.uname + " Password: " + post.psw);
-                    // intervalID is necessary to stop the interval with clearInterval()
-                    var userData = { "userName": post.uname, "lastInteraction": calculateTimestamp(), "intervalID": '' };
-                    usersInSession.push(userData);
-                    triggerSession(userData);
+                    // generate jwt key
+                    var jwtKey = jwt.sign({ 'name': post.uname }, 'sh1bb0l3th', { expiresIn: '7d' });
+                    console.log('JWT generated: ' + token);
+                    // return the user to the web page selected above (html/add.html):
+                    // add to the document a script with a jwt as login key
+                    res.writeHead(200, { 'content-type': 'text/html' });
+                    res.write(data + "<script>localStorage.setItem('key', '" + jwtKey + "');</script>");
+                    console.log("Sending login script back");
+                    return res.end();
                 }
             });
-            // return the user to the web page selected above (html/add.html):
-            res.writeHead(200, { 'content-type': 'text/html' });
-            res.write(data);
-            return res.end();
+
         });
     };
 
     // test if the root domain was selected, or index without its extension was entered
     // if the above is true, return index.html
-    // this will not include queries, as queries will alway have .html added
+    // this will not include queries, as queries will always have .html added
     if ((req.url === "/" || req.url === "/index") && req.method === "GET") {
         fs.readFile("index.html", function(err, data) {
             if (err) {
@@ -351,7 +358,5 @@ http.createServer(function(req, res) {
             return res.end();
         });
     }
-
-
 
 }).listen(8080);
