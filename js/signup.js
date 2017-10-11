b6e13b59203e45f0078b28a84a25523c6e284597
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-10-10 23:01:01 -0400

    added basic sign up form (email and password) client side validation and page

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 
+
+    var email = $('#signup_email');
+    var emailVal = $('#signup_email_val');
+    var psw = $('#signup_psw');
+    var pswVal = $('#signup_psw_val');
+
+    // check to see if both emails entered for usernames are the same
+    function validateEmail() {
+        if (email.val() != emailVal.val()) {
+            emailVal.append("<script>document.getElementById('signup_email').setCustomValidity('Emails do not match!');");
+        } else {
+            emailVal.append("<script>document.getElementById('signup_email').setCustomValidity('');");
+        }
+    }
+
+    // check to see if both passwords entered are the same
+    function validatePsw() {
+        if (psw.val() != pswVal.val()) {
+            pswVal.append("<script>document.getElementById('signup_psw_val').setCustomValidity('Passwords do not match!');");
+        } else {
+            pswVal.append("<script>document.getElementById('signup_psw_val').setCustomValidity('');");
+        }
+    }
+
+    psw.blur(validatePsw);
+    pswVal.blur(validatePsw);
+
+    email.blur(validateEmail);
+    emailVal.blur(validateEmail);
+
 });
