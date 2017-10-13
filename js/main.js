09c9f73392385e476d721ee1722f236fb331feba
Ronald van der Bergh <rhvdbergh@gmail.com>
2017-10-12 22:32:29 -0400

    login error messages added

--- js/main.js
+++ js/main.js
@@ -2,19 +2,12 @@ $(document).ready(function() {
     var loggedIn = false;
     var key = localStorage.getItem('key');
     var currenttable = localStorage.getItem('currenttable');
-
-    $('.submit_login_btn').on('click', function() {
-
-        // send login details to the server
-        // at this stage, the server will create a new user if no email address
-        // exists; in the future, there will be a sign-up form
-        console.log('Submitted!');
-        // check to see if the login is valid
-    });
+    var loginValidationFailCode = 999;
 
     if (key != null) { // if there is no key, leave loggedIn = false
 
         $.get("?validateJWT" + key, function(data) {
+            loginValidationFailCode = data;
             if (data === "0") { // there is something wrong with the token. New login and token required
                 localStorage.removeItem('key'); // key removed so new key will be generated
                 console.log("User token existed, but was corrupt. Token removed & new login required.");
@@ -24,6 +17,11 @@ $(document).ready(function() {
             }
             if (data === "2") { // token has expired
                 console.log("Token has expired. New login required.");
+                localStorage.removeItem('key'); // key removed so new key will be generated
+            }
+            if (data === "3") {
+                console.log("User does not exist or wrong password entered.");
+                localStorage.removeItem('key'); // key removed so new key will be generated
             }
             doLoginLogic();
         });
@@ -36,6 +34,23 @@ $(document).ready(function() {
         if (!loggedIn) {
             $('.login_container').append("<input type='email' placeholder='Email' name='uname' required>");
             $('.login_container').append("<input type='password' placeholder='Password' name='psw' required>");
+
+            console.log(loginValidationFailCode);
+            switch (loginValidationFailCode) {
+                case "0":
+                    $('.login_container').append("<p class = 'errmsg centered'>Login failed. Please try again.</p>");
+                    break;
+                case "2":
+                    $('.login_container').append("<p class = 'errmsg centered'>Your session has expired. Please try again.</p>");
+                    break;
+                case "3":
+                    $('.login_container').append("<p class = 'errmsg centered'>User does not exist or wrong password entered.</p>");
+                    break;
+                default:
+                    break;
+            }
+            //   $('.login_container').append("<p class = 'errmsg centered'>Login failed. Please try again.</p>");
+
             $('.login_container').append("<button type='submit' class='submit_login_btn' name='login_btn' value='login_btn'>Login</button>");
             $('.login_container').append("<p class='centered'>or</p>");
             $('.signup_container').append("<a href='./html/signup.html' class='submit_signup_btn'>Sign up</a>");
@@ -51,7 +66,7 @@ $(document).ready(function() {
             $('.navbar-right').append("<li class='logout'><a href='./index.html'>Logout</a></li><script>$('.logout').on('click', function() {localStorage.removeItem('key');});</script>");
             //  $('.navbar-right').append("<li id='nav_btn_account'><a href=''>Account</a></li>");
             $('.login_and_signup_container').hide();
-            if (currenttable === null || currenttable != "" || currenttable != "none") { // the user doesn't appear to have any tables, and no table is selected
+            if (currenttable === null || currenttable != "" || currenttable != "null") { // the user doesn't appear to have any tables, and no table is selected
                 $('.create_table_container').show();
                 $('.create_table_container').append("<button type='submit' class='create_table_btn' name='create_table_btn' value='create_table_btn'>Create a new set of cards</script>");
             }
